17d63729e4ea369d68612eee66f503caf7ae0dd1
nuhs <58536161+nuhs@users.noreply.github.com>
2019-12-05 12:41:13 +0900

    Update open3d_pra.py

--- open3d_pra.py
+++ open3d_pra.py
@@ -5,7 +5,7 @@ from farmware_tools import device, get_config_value, env
 import cv2
 import numpy as np
 
-device.log(message='OK!', message_type='success')
+device.log(message='OK! Numpy', message_type='success')
 #import open3d as o3d
 #import os
 #from time import time, sleep

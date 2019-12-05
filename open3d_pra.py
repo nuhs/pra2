f3bb27857e9b5e131f235c52fc55181aa2984939
nuhs <58536161+nuhs@users.noreply.github.com>
2019-12-05 12:38:02 +0900

    Update open3d_pra.py

--- open3d_pra.py
+++ open3d_pra.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import cv2
+from farmware_tools import device, get_config_value, env
+import cv2
+
 device.log(message='OK!', message_type='success')
 #import numpy as np
 #import open3d as o3d
 #import os
 #from time import time, sleep
-#from farmware_tools import device, get_config_value, env
 
 #config = o3d.io.AzureKinectSensorConfig()

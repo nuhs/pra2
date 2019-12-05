bd37f429fa946a9c328b52939f9a84e8e84d15a7
nuhs <58536161+nuhs@users.noreply.github.com>
2019-12-05 17:13:56 +0900

    Update open3d_pra.py

--- open3d_pra.py
+++ open3d_pra.py
@@ -14,38 +14,60 @@ from time import time, sleep
 #import pykinect
 
 device.log(message='OK!', message_type='success')
-# Open the camera
-#p = os.getenv('camera', 'USB')
-camera = cv2.VideoCapture(0)
-sleep(0.1)
-device.log(message='camera', message_type='info')
 
-epoch = int(time())
-filename = '{timestamp}.jpg'.format(timestamp=epoch)
-images_dir = env.Env().images_dir or '/tmp/images'
-if not os.path.isdir(images_dir):
-    device.log('{} directory does not exist.'.format(images_dir), 'error')
-path = images_dir + os.sep + filename
-discard_frames = 10
-for _ in range(discard_frames):
-  camera.grab()
+def image_filename():
+    'Prepare filename with timestamp.'
+    epoch = int(time())
+    filename = '{timestamp}.jpg'.format(timestamp=epoch)
+    return filename
 
-# Take a photo
-ret, image = camera.read()
-# Close the camera
-camera.release()
+def upload_path(filename):
+    'Filename with path for uploading an image.'
+    images_dir = env.Env().images_dir or '/tmp/images'
+    if not os.path.isdir(images_dir):
+        device.log('{} directory does not exist.'.format(images_dir), 'error')
+    path = images_dir + os.sep + filename
+    return path
 
-if ret:  # an image has been returned by the camera
-    filename_path = upload_path(image_filename())
-    # Save the image to file
-    cv2.imwrite(filename_path, image)
-    print('Image saved: {}'.format(filename_path))
-else:  # no image has been returned by the camera
-    device.log('Problem getting image from video{}.'.format(
-        camera_port), 'error', ['toast'])
-if ret:
-  h, w, c = image.shape
-  device.log(message='h={},w={}'.format(h,w), message_type='info')
-  device.log(message='c={}'.format(c), message_type='info')
+def usb_camera_photo(camera_port):
+    'Take a photo using a USB camera.'
+    # Settings
+    discard_frames = 10  # number of frames to discard for auto-adjust
+
+    # Check for camera
+    if not os.path.exists('/dev/video' + str(camera_port)):
+        print('No camera detected at video{}.'.format(camera_port))
+        device.log('USB Camera at video{} not detected.'.format(
+            camera_port), 'error', ['toast'])
+
+    # Open the camera
+    camera = cv2.VideoCapture(camera_port)
+    sleep(0.1)
+
+    # Let camera adjust
+    for _ in range(discard_frames):
+        camera.grab()
+
+    # Take a photo
+    ret, image = camera.read()
+
+    # Close the camera
+    camera.release()
+
+    # Output
+    if ret:  # an image has been returned by the camera
+        filename_path = upload_path(image_filename())
+        # Save the image to file
+        #cv2.imwrite(filename_path, image)
+        print('Image saved: {}'.format(filename_path))
+        h, w, c = image.shape
+        
+    else:  # no image has been returned by the camera
+        device.log('Problem getting image from video{}.'.format(
+            camera_port), 'error', ['toast'])
+
+if __name__ == '__main__':
+    usb_camera_photo(0)
+
 
 #config = o3d.io.AzureKinectSensorConfig()

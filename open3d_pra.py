2a4caa12ea63e090352e7be3c2306a85da9d2805
nuhs <58536161+nuhs@users.noreply.github.com>
2019-12-05 12:52:46 +0900

    Update open3d_pra.py

--- open3d_pra.py
+++ open3d_pra.py
@@ -6,9 +6,10 @@ import cv2
 import numpy as np
 import os
 import sys
+import datetime
 #import open3d as o3d
 #import pykinect
 
-device.log(message='OK! sys', message_type='success')
+device.log(message='OK! datetime', message_type='success')
 
 #config = o3d.io.AzureKinectSensorConfig()
